fbc3d8e240291ccc7c4ced7d9ae962d8407c4da0
J. Nick Koston <nick@koston.org>
2022-11-14 13:01:46 -0600

    feat: move BluetoothServiceInfoBleak out of the bluetooth integration (#10)

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,12 @@
+import time
+
 from bleak.backends.device import BLEDevice
 
-from home_assistant_bluetooth import SOURCE_LOCAL, BluetoothServiceInfo
+from home_assistant_bluetooth import (
+    SOURCE_LOCAL,
+    BluetoothServiceInfo,
+    BluetoothServiceInfoBleak,
+)
 
 from . import generate_advertisement_data
 
@@ -47,3 +53,37 @@ def test_model_from_bleak():
     assert service_info.source == SOURCE_LOCAL
     assert service_info.manufacturer is None
     assert service_info.manufacturer_id is None
+
+
+def test_model_from_scanner():
+    switchbot_device = BLEDevice("44:44:33:11:23:45", "wohand")
+    switchbot_adv = generate_advertisement_data(
+        local_name="wohand", service_uuids=["cba20d00-224d-11e6-9fb8-0002a5d5c51b"]
+    )
+    now = time.monotonic()
+    service_info = BluetoothServiceInfoBleak.from_scan(
+        SOURCE_LOCAL, switchbot_device, switchbot_adv, now, True
+    )
+
+    assert service_info.service_uuids == ["cba20d00-224d-11e6-9fb8-0002a5d5c51b"]
+    assert service_info.name == "wohand"
+    assert service_info.source == SOURCE_LOCAL
+    assert service_info.manufacturer is None
+    assert service_info.manufacturer_id is None
+    assert service_info.time == now
+    assert service_info.connectable is True
+
+    safe_as_dict = service_info.as_dict()
+    assert safe_as_dict == {
+        "address": "44:44:33:11:23:45",
+        "advertisement": switchbot_adv,
+        "device": switchbot_device,
+        "connectable": True,
+        "manufacturer_data": {},
+        "name": "wohand",
+        "rssi": -127,
+        "service_data": {},
+        "service_uuids": ["cba20d00-224d-11e6-9fb8-0002a5d5c51b"],
+        "source": "local",
+        "time": now,
+    }
